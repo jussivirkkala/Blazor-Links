89696e56c915da2607cc11685b6f1ae5d9b13207
Jussi Virkkala <jussivirkkala@hotmail.com>
2023-08-09 07:20:22 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using Blazor_Links;
-using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,7 +7,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddBlazoredLocalStorage(); 
 
 await builder.Build().RunAsync();
